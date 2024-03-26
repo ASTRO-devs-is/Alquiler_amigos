8654ff377423e1f2312e40d4f38f7ae73bdc3427
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-03-26 08:46:59 -0400

    feat: caja de texto programar salida

--- alquilarAmigo/forms.py
+++ alquilarAmigo/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 categorias = [('Casual', 'Casual'), ('Deporte', 'Deporte'), ('Cultura', 'Cultura'), ('Cosplay', 'Cosplay'), ('Formal', 'Formal'), ('Virtual', 'Virtual')]
 
-class formularioCategoria(forms.Form):
+class formularioProgramarCita(forms.Form):
     categorias = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), 
                                     choices=categorias, label='Escoge la categoria de tu Salida', required=True)
+    cajaTexto = forms.CharField(required=True, label='Escribe la descripcion de tu salida', max_length=500, widget=forms.Textarea(attrs={'class': 'form-control'}))
