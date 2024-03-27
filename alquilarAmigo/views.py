2933a86b7f9064d4a30c161e25fe4d7a199b1dc2
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-03-27 16:38:04 -0400

    actualizacion

--- alquilarAmigo/views.py
+++ alquilarAmigo/views.py
@@ -4,4 +4,15 @@ from .forms import formularioProgramarCita
 
 def programarSalida(request):
     formulario_categoria = formularioProgramarCita()
+    if request.method == 'POST':
+        #formulario_salida = formularioProgramarCita(request.POST)#metemos la informacion de los cuadros al formulario
+        
+        categorias = request.POST.get('categorias')
+        fecha = request.POST.get('fecha')
+        cajaTexto = request.POST.get('cajaTexto')
+        horaInicio = request.POST.get('horaInicio')
+        horaFin = request.POST.get('horaFin')
+        datos = {'categorias': categorias, 'fecha': fecha, 'cajaTexto': cajaTexto, 'horaInicio': horaInicio, 'horaFin': horaFin}
+        return render(request, "programarSalida/respuesta.html", {'datos': datos})
+                
     return render(request, 'programarSalida/programarSalida.html', {'formCateg': formulario_categoria})
