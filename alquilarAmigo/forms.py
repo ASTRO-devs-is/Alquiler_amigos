9df1f81800ea6764a06885d9831d10282a7bc955
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-03-29 11:41:03 -0400

    feat: validaciones iniciales en programar cita

--- alquilarAmigo/forms.py
+++ alquilarAmigo/forms.py
@@ -1,17 +1,53 @@
+import datetime
 from django import forms
+from .models import Salida
+
+
 
 categorias = [('Casual', 'Casual'), ('Deporte', 'Deporte'), ('Cultura', 'Cultura'), ('Cosplay', 'Cosplay'), ('Formal', 'Formal'), ('Virtual', 'Virtual')]
 
 class formularioProgramarCita(forms.Form):
-    categorias = forms.ChoiceField(widget=forms.Select(attrs={'class': 'form-control'}), 
-                                    choices=categorias, label='Escoge la categoria de tu Salida', required=True)
-    cajaTexto = forms.CharField(required=True, label='Escribe la descripcion de tu salida', max_length=500, widget=forms.Textarea(attrs={'class': 'form-control'}))
-    fecha = forms.DateField(label='Escoge la fecha de tu salida', required=True,
-                            widget=forms.DateInput(format="%d-%m-%Y", attrs={"type": "date",'class': 'form-control'}),
-                            input_formats=["%d-%m-%Y"])
-    horaInicio = forms.TimeField(label='Escoge la hora de Inicio', required=True, 
-                            widget=forms.TimeInput(format="%H-%M", attrs={"type": "time",'class': 'form-control'}),
-                            input_formats=["%H-%M"])
-    horaFin = forms.TimeField(label='Escoge la hora de fin', required=True, 
-                            widget=forms.TimeInput(format="%H-%M", attrs={"type": "time",'class': 'form-control'}),
-                            input_formats=["%H-%M"])
+    categorias = forms.ChoiceField(choices=categorias, label='Escoge la categoria de tu Salida', required=False,
+                                    widget=forms.Select(attrs={'class': 'form-control'}))
+    cajaTexto = forms.CharField(label='Escribe la descripcion de tu salida',required=False,
+                                widget=forms.Textarea(attrs={'class': 'form-control'}))
+    fecha = forms.DateField(label='Escoge la fecha de tu salida', required=False,
+                            widget=forms.DateInput( attrs={"type": "date",'class': 'form-control'}))
+    horaInicio = forms.TimeField(label='Escoge la hora de Inicio',required=True,
+                                widget=forms.TimeInput( attrs={"type": "time",'class': 'form-control'}))
+    horaFin = forms.TimeField(label='Escoge la hora de fin',required=False,
+                            widget=forms.TimeInput(attrs={"type": "time",'class': 'form-control'}))
+    
+    def clean_cajaTexto(self):
+        cajaTexto = self.cleaned_data['cajaTexto']
+        if cajaTexto == None or cajaTexto == '': 
+            raise forms.ValidationError('La descripcion no puede estar vacia')
+        return cajaTexto
+
+    def clean_fecha(self):
+        fecha = self.cleaned_data['fecha']
+        if fecha == None:
+            raise forms.ValidationError('La fecha no puede estar vacia')
+        if fecha < datetime.date.today():
+            raise forms.ValidationError('La fecha no puede ser menor a la fecha actual')
+        if fecha > datetime.date.today() + datetime.timedelta(weeks=20):
+            raise forms.ValidationError('La fecha no puede ser mayor a 20 semanas de la fecha actual')
+        return fecha
+    
+    def clean_horaInicio(self):
+        horaInicio = self.cleaned_data['horaInicio']
+        if horaInicio == None:
+            raise forms.ValidationError('La hora de inicio no puede estar vacia')
+        if horaInicio < datetime.datetime.now().time():
+            raise forms.ValidationError('La hora de inicio no puede ser menor a la hora actual')
+        return horaInicio
+
+    def clean_horaFin(self):
+        horaFin = self.cleaned_data['horaFin']
+        if horaFin == None:
+            raise forms.ValidationError('La hora de fin no puede estar vacia')
+        if horaFin < datetime.datetime.now().time():
+            raise forms.ValidationError('La hora de fin no puede ser menor a la hora actual')
+        return horaFin
+    
+    
--- alquilarAmigo/views.py
+++ alquilarAmigo/views.py
@@ -1,18 +1,23 @@
 from django.shortcuts import render
 from .forms import formularioProgramarCita
+import datetime
 # Create your views here.
 
 def programarSalida(request):
-    formulario_categoria = formularioProgramarCita()
+    formulario_programarSalida = formularioProgramarCita()
     if request.method == 'POST':
-        #formulario_salida = formularioProgramarCita(request.POST)#metemos la informacion de los cuadros al formulario
-        
-        categorias = request.POST.get('categorias')
-        fecha = request.POST.get('fecha')
-        cajaTexto = request.POST.get('cajaTexto')
-        horaInicio = request.POST.get('horaInicio')
-        horaFin = request.POST.get('horaFin')
-        datos = {'categorias': categorias, 'fecha': fecha, 'cajaTexto': cajaTexto, 'horaInicio': horaInicio, 'horaFin': horaFin}
-        return render(request, "programarSalida/respuesta.html", {'datos': datos})
+        formulario_datos = formularioProgramarCita(request.POST)
+        if formulario_datos.is_valid():
+            #formulario_datos.save()
+            categorias = request.POST.get('categorias')
+            fecha = request.POST.get('fecha')
+            cajaTexto = request.POST.get('cajaTexto')
+            horaInicio = request.POST.get('horaInicio')
+            horaFin = request.POST.get('horaFin')
+            datos = {'categorias': categorias, 'fecha': fecha, 'cajaTexto': cajaTexto, 'horaInicio': horaInicio, 'horaFin': horaFin}
+            return render(request, "programarSalida/respuesta.html", {'datos': datos})
+        else:
+            return render(request, 'programarSalida/programarSalida.html', {'formSalida': formulario_datos, 'errores': formulario_datos.errors})
+            
                 
-    return render(request, 'programarSalida/programarSalida.html', {'formCateg': formulario_categoria})
+    return render(request, 'programarSalida/programarSalida.html', {'formSalida': formulario_programarSalida})
