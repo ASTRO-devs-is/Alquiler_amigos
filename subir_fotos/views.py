7833c82ddaced47cc294a9621e5fb93491dd518e
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-04-12 22:30:52 -0400

    fix:decodificando descripcion

--- subir_fotos/views.py
+++ subir_fotos/views.py
@@ -2,15 +2,17 @@ from django.shortcuts import render, redirect
 from .models import FotoPerfil
 from alquilarAmigo.models import Amigo, Direccion, Tarifa
 from subir_fotos.models import FotoPerfil
+from urllib.parse import unquote
 
 
 
 def cargar_fotos_perfil(request, nombre, apellido, ciudad, pais, telefono, email, localidad, descripcion, fecha, tarifa, genero):
 
-    datos = []
+    
     if request.method == 'POST':
         
         # Obtener archivos subidos
+        descripcion_decodificada = unquote(descripcion)
         uploaded_files = request.FILES.getlist('file-input')
         tarifa = Tarifa.objects.get(tarifa=tarifa)
         direccion = Direccion(ciudad=ciudad, pais=pais, localidad=localidad)
@@ -18,7 +20,7 @@ def cargar_fotos_perfil(request, nombre, apellido, ciudad, pais, telefono, email
         id_ubicacion = direccion.id
         amigo = Amigo(nombre=nombre, apellido=apellido, telefono=telefono, 
                     ubicacion_id= id_ubicacion, correo=email,
-                    descripcion=descripcion, fecha_nacimiento=fecha, id_tarifa=tarifa, genero = genero)
+                    descripcion=descripcion_decodificada, fecha_nacimiento=fecha, id_tarifa=tarifa, genero = genero)
         amigo.save()
         
         for uploaded_file in uploaded_files:
@@ -28,7 +30,7 @@ def cargar_fotos_perfil(request, nombre, apellido, ciudad, pais, telefono, email
     else:
         return render(request, 'formulario.html') # Renderizar la plantilla del formulario de subida
     
-def cancelar_subir_fotos(request, datos=None):
+def cancelar_subir_fotos(request):
     return render(request, 'cancelar.html')
 
     
