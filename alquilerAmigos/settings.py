9c3244d1f7527be2a35766a69910e76fa89edef6
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-04-28 12:54:56 -0400

    feat: funcionalidad de los botones iniciar sesion y cerrar sesion

--- alquilerAmigos/settings.py
+++ alquilerAmigos/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+#para poder cerrar sesion
+LOGOUT_REDIRECT_URL = 'inicio_login'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
