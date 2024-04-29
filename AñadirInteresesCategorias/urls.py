a5fdd3dc55409eb0d2f30851c2c3e9d8231504db
ErikaCA99 <163912299+ErikaCA99@users.noreply.github.com>
2024-04-29 16:38:21 -0400

    feat: mejoras del fronted y conexion con el backed

--- AñadirInteresesCategorias/urls.py
+++ AñadirInteresesCategorias/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('guardar-intereses-categorias/', views.guardar_intereses_categorias, name='guardar_intereses_categorias'),
-    # Otras URLs de tu aplicación
 ]
