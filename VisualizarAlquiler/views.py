b864907e5f66599d65a3733ba1a67bc286ee41ed
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-05-07 20:20:23 -0400

    feat: terminando pla plantilla de listar alquileres

--- VisualizarAlquiler/views.py
+++ VisualizarAlquiler/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from alquilarAmigo.models import Salida, Amigo, User
 
 # Create your views here.
 def visualizarAlquiler(request):
-    return render(request, 'VisualizarAlquiler/visualizarAlquiler.html')
+    usuario = User.objects.get(id=request.user.id)
+    try:
+        usuarioAmigo = Amigo.objects.get(correo=usuario.email)
+    except:
+        usuarioAmigo = None
+    if(usuarioAmigo != None):
+        salidas = Salida.objects.filter(amigo_id=usuarioAmigo.id)
+        amigoTieneSalida = True
+        
+    else:
+        amigoTieneSalida = False
+    
+    amigo = {'salidas': salidas, 'tieneSalida': amigoTieneSalida}
+    
+
+    return render(request, 'VisualizarAlquiler/visualizarAlquiler.html', amigo)
