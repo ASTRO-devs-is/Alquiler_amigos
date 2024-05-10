3bff94114a5d3e1871cfa30215be8cce4a7c08d7
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-05-09 21:47:36 -0400

    feat: card ya se puede visualizar la imagen del cliente

--- VisualizarAlquiler/views.py
+++ VisualizarAlquiler/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
 from alquilarAmigo.models import Salida, Amigo, User
+from subir_fotos.models import FotoPerfil
 
 # Create your views here.
 def visualizarAlquiler(request):
-    usuario = User.objects.get(id=request.user.id)
+    usuarioA = User.objects.get(id=request.user.id)
+    
     try:
-        usuarioAmigo = Amigo.objects.get(correo=usuario.email)
+        usuarioAmigo = Amigo.objects.get(correo=usuarioA.email)
     except:
         usuarioAmigo = None
     if(usuarioAmigo != None):
         salidas = Salida.objects.filter(amigo_id=usuarioAmigo.id)
         amigoTieneSalida = True
-        
+        salidas_con_fotos = [{'salida': salida, 'foto': FotoPerfil.objects.filter(fotosCliente__id=salida.cliente.id).first()} for salida in salidas]
     else:
         amigoTieneSalida = False
     
-    amigo = {'salidas': salidas, 'tieneSalida': amigoTieneSalida}
+    #print(salidas)
+    print(salidas_con_fotos)
     
-    #print (amigo)
-    return render(request, 'VisualizarAlquiler/visualizarAlquiler.html', amigo)
+    return render(request, 'VisualizarAlquiler/visualizarAlquiler.html', {'contexto': {'salidas_con_fotos': salidas_con_fotos, 'tieneSalida': amigoTieneSalida}})
