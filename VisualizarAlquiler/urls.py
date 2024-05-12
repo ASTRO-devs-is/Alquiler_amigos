53229eabf895f58c31f3a7b9f37c30a94f5b50f4
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-05-11 21:13:12 -0400

    feat: cambio de estado backend

--- VisualizarAlquiler/urls.py
+++ VisualizarAlquiler/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.visualizarAlquiler, name='visualizarAlquiler'),
+    path('cambiar_estado_salida/', views.cambiar_estado_salida, name='cambiar_estado_salida'),
 ]
--- VisualizarAlquiler/views.py
+++ VisualizarAlquiler/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from alquilarAmigo.models import Salida, Amigo, User
 from subir_fotos.models import FotoPerfil
+from django.http import JsonResponse
 
 # Create your views here.
 def visualizarAlquiler(request):
@@ -17,7 +18,16 @@ def visualizarAlquiler(request):
     else:
         amigoTieneSalida = False
     
-    #print(salidas)
-    print(salidas_con_fotos)
-    
     return render(request, 'VisualizarAlquiler/visualizarAlquiler.html', {'contexto': {'salidas_con_fotos': salidas_con_fotos, 'tieneSalida': amigoTieneSalida}})
+
+
+def cambiar_estado_salida(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        estado = request.POST.get('estado')
+        salida = Salida.objects.get(id=id)
+        salida.estado = estado
+        salida.save()
+        return JsonResponse({'success': True})
+    else:
+        return JsonResponse({'success': False})
