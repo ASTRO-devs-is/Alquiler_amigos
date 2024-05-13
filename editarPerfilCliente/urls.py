2c54f9844326974b50022411d9fc059dd81e6acc
CeciJhoana <ceciliavergara997@gmail.com>
2024-05-13 17:05:42 -0400

    Change[url]: path de editar cliente

--- editarPerfilCliente/urls.py
+++ editarPerfilCliente/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.editar_datos_cliente, name='editarPerfilCliente'),
+    path('<int:id_usuario>/', views.editar_datos_cliente, name='editarPerfilCliente'),
     path('actualizar_datos/', views.actualizar_datos, name='actualizar_datos'),
 ]
