f8b13e9aa696e7e3a0fa9820e347bdefa67480c1
CeciJhoana <ceciliavergara997@gmail.com>
2024-05-13 20:00:21 -0400

    Change[views]: actualizar datos

--- editarPerfilCliente/views.py
+++ editarPerfilCliente/views.py
@@ -15,7 +15,8 @@ def actualizar_datos (request,id_usuario):
     cliente = get_object_or_404(Cliente, id=id_usuario)
 
     if request.method == 'POST':
-        form = ClienteForm(request.POST)
+        form = ClienteForm(request.POST, instance=cliente)
+        #form = ClienteForm(request.POST)
         #print(form.is_valid())
         if form.is_valid():
             telefono = form.cleaned_data['telefono']
@@ -24,20 +25,22 @@ def actualizar_datos (request,id_usuario):
                 pais=form.cleaned_data['pais'],
                 ciudad=form.cleaned_data['ciudad'],
                 localidad=form.cleaned_data['localidad'],
-                correo=form.cleaned_data['email']
+                
             )
             cliente.ubicacion = direccion
             #Verificamos si el telefono existe en la base de datos
-            if Cliente.objects.filter(telefono=telefono).exists():
+            #if Cliente.objects.filter(telefono=telefono).exists():
+            if Cliente.objects.exclude(id=id_usuario).filter(telefono=telefono).exists():
                 return render(request, 'editarPerfilCliente.html', {'form': form, 'telefonoRepetido': 'Este telefono ya esta registrado'})
-            elif Cliente.objects.filter(correo=direccion.correo).exists():
-                return render(request, 'editarPerfilCliente.html', {'form': form, 'correoRepetido': 'Este correo ya esta registrado'})
+            #elif Cliente.objects.filter(correo=direccion.correo).exists():
+            #    return render(request, 'editarPerfilCliente.html', {'form': form, 'correoRepetido': 'Este correo ya esta registrado'})
             else:
                 cliente.save()
                 return redirect('Inicio')
     
     else:
 
-        form = ClienteForm()
+        #form = ClienteForm()
+        form = ClienteForm(instance=cliente)
     return render(request, 'editarPerfilCliente.html', {'form': form})
  
