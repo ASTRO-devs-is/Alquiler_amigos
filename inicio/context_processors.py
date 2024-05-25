cd4f74f7c3884af455e4f95f342f61e9fc8e2cb3
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-05-24 21:35:12 -0400

    feat: conexiones necesarias

--- inicio/context_processors.py
+++ inicio/context_processors.py
@@ -1,22 +1,23 @@
-from alquilarAmigo.models import User, Amigo
+from alquilarAmigo.models import User, Amigo, Cliente
 
 def usuarioLog(request):
+    amigo = {'esAmigo': False}
     if request.user.is_authenticated:
         usuario = User.objects.get(id=request.user.id)
         try:
             usuarioAmigo = Amigo.objects.get(correo=usuario.email)
-        except:
-            usuarioAmigo = None
-        if(usuarioAmigo != None):
-            amigo = {
-                'nombre': usuarioAmigo.nombre,
-                'esAmigo': True,
-            }
-        else:
-            amigo = {
-                'esAmigo': False,
-            }
-        
+            if usuarioAmigo is not None:
+                amigo['amigo'] = usuarioAmigo
+                amigo['esAmigo'] = True
+        except Amigo.DoesNotExist:
+            pass
+
+        try:
+            usuarioCliente = Cliente.objects.get(correo=usuario.email)
+            if usuarioCliente is not None:
+                amigo['cliente'] = usuarioCliente
+        except Cliente.DoesNotExist:
+            pass
     else:
         amigo = None
 
